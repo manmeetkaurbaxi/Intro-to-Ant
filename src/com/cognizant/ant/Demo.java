47a54effcb4a274956bcfd525fb2afe369d8a739
Manmeet Kaur <manmeetkaur0175@gmail.com>
2020-01-29 15:23:13 +0530

    Adding project files

---
+++ src/com/cognizant/ant/Demo.java
@@ -0,0 +1,14 @@
+package com.cognizant.ant;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo File compiled...");
+		System.out.println("All tasks executed completely...");
+//		System.out.println("Welcome to ANT Environment... :)");
+//		System.out.println("Reading JAVA_HOME Enviornment Variable...");
+//        System.out.println("JAVA_HOME Value:- " + System.getenv("JAVA_HOME"));
+	}
+
+}
